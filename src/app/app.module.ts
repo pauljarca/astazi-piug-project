f5ba054df49ae42b29d76f3fdccf2b735e9c1b04
Paul <paulvjarca@gmail.com>
2021-11-22 16:09:10 +0200

    Created the project, worked on carousel and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageCarouselComponent } from './homepage-carousel/homepage-carousel.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageCarouselComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
